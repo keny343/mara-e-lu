fde54e448f5cf4f3a052853bc565b112b2c2e0db
Adnircio Inocencio <kenydelrey8@gmail.com>
2026-02-13 13:42:56 +0100

    Corrigindo CORS para frontend mara-e-lu.up.railway.app

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const app = express();
 
 // Middlewares
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ['https://mara-e-lu.up.railway.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
