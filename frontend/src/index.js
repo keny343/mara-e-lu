0cc20ecafc877a44b512115c048d0a1ed18ed2df
Adnircio Inocencio <kenydelrey8@gmail.com>
2026-02-21 15:41:46 +0100

    Add missing pages and fix package-lock.json sync for Railway deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
